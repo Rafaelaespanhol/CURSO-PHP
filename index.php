73c9d179b653ef4f535c1ae05f9e82356f903dbf
Rafaela Espanhol <117127231+Rafaelaespanhol@users.noreply.github.com>
2022-11-11 22:05:29 -0300

    Informações php

--- index.php
+++ index.php
@@ -13,13 +13,21 @@ print_r('<h2>escrevendo de novo</h2>');
 $nome = 'Rafaela';
 $idade = 24;
 $altura = 1.55;
-$fumante = true;
+$fumante = false;
 
 echo'<br>';
 echo $nome . '<br>';
 echo $idade . '<br>';
 echo $altura .'<br>';
 echo $fumante . '<br>';
-echo'Olá'. $nome  . 'você tem' . $idade .  'anos';
+echo 'Olá' . $nome . ' você tem ' . $idade .'anos';
+
+echo '<hr>';
+echo gettype ($nome) . '<br>';
+echo gettype ($idade) . '<br>';
+echo gettype ($altura) . '<br>';
+echo gettype ($fumante) . '<br>';
+$altura = '1.75';
+echo gettype($altura) . '<br>';
 
 ?> 
